b9860c9abd82b38a813fc22b241c8b5bac445a42
David Blackwelder <github@davidblackwelder.com>
2024-02-06 09:43:38 -0500

    Add unit tests and screenshot of passing tests

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,91 @@
 import pytest
-# TODO: add necessary import
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+from ml.data import process_data
+from ml.model import (compute_model_metrics, inference, load_model, performance_on_categorical_slice, save_model, train_model)
 
+def test_rows_in_dataset():
+  """
+  # Ensure number of rows in dataset is correct
+  """
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+  data_path = './data/census.csv'
+  data = pd.read_csv(data_path)
 
+  expected_rows = 32561
+  actual_rows = data.shape[0]
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+  assert actual_rows == expected_rows
+
+def test_cols_in_dataset():
+  """
+  # Ensure number of columns in dataset is correct
+  """
+
+  data_path = './data/census.csv'
+  data = pd.read_csv(data_path)
+
+  expected_cols = 15
+  actual_cols = data.shape[1]
+
+  assert actual_cols == expected_cols
+
+def test_model_type_is_forest_classifier():
+  """
+  # Ensure the model is a forest_classifier type
+  """
+
+  data_path = './data/census.csv'
+  data = pd.read_csv(data_path)
+
+  # split the provided data to have a train dataset and a test dataset
+  # Optional enhancement, use K-fold cross validation instead of a train-test split.
+  train, test = train_test_split(data, test_size=0.2)
+
+  cat_features = [
+      "workclass",
+      "education",
+      "marital-status",
+      "occupation",
+      "relationship",
+      "race",
+      "sex",
+      "native-country"
+  ]
+
+  # process the training data.
+  X_train, y_train, encoder, lb = process_data(
+      # use the train dataset
+      # use training=True
+      # do not need to pass encoder and lb as input
+      train,
+      categorical_features=cat_features,
+      label='salary',
+      training=True
+      )
+
+  X_test, y_test, _, _ = process_data(
+      test,
+      categorical_features=cat_features,
+      label="salary",
+      training=False,
+      encoder=encoder,
+      lb=lb,
+  )
+
+  # train the model on the training dataset
+  model = train_model(X_train, y_train)
+
+  assert isinstance(model, RandomForestClassifier)
+
+def test_slice_output_is_not_empty():
+  """
+  # Ensure the slice_output.txt file is not empty
+  """
+
+  with open('slice_output.txt', 'r') as file:
+    content = file.read()
+
+  assert len(content) > 0
